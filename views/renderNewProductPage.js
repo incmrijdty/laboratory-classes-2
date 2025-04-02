80435ff2ffb3ee4928d5669e15e5363b0ff5594f
incmrijdty <alesia.sichova@yandex.by>
2025-04-02 15:48:36 +0200

    Update renderNewProductPage.js

--- views/renderNewProductPage.js
+++ views/renderNewProductPage.js
@@ -1,5 +1,30 @@
 /* 🏗 Structo the Builder */
 /* Funkcja generująca stronę HTML z najnowszym produktem */
-const renderNewProductPage = () => {};
+const fs = require('fs');
+
+const renderNewProductPage = (response) => {
+  fs.readFile("./product.txt", "utf-8", (err, data) => {
+    response.setHeader("Content-Type", "text/html");
+    response.write("<html>");
+    response.write("<head><title>Shop - Newest product</title></head>");
+    response.write("<body>");
+    response.write("<h1>Newest product</h1>");
+    response.write(
+      "<nav><a href='/'>Home</a><br /><a href='/product/add'>Add product</a><br /><a href='/logout'>Logout</a></nav>"
+    );
+
+    if (err || !data) {
+      response.write("<br /><div>No new products available.</div>");
+    } else {
+      const splittedData = data.split('\n');  
+      response.write(`<br /><div>New product data - ${splittedData}</div>`);
+    }
+
+    response.write("</body>");
+    response.write("</html>");
+
+    return response.end();
+  });
+};
 
 module.exports = renderNewProductPage;

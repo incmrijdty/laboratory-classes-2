e1e822e8fc916831d5e4091c015aba6fc9dc47b5
incmrijdty <alesia.sichova@yandex.by>
2025-04-02 15:12:07 +0200

    Update kill.js

--- routing/kill.js
+++ routing/kill.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const logger = require('../utils/logger');
+
+
+router.get('/', (req, res) => {
+  logger.getProcessLog();  
+  //res.status(200).send('Process has been terminated.');  
+  process.exit();  
+});
+
+module.exports = router;
 
